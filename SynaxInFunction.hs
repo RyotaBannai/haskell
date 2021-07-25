3f47a9c3324200f4d90c4b2ffae18489ee7ede85
Ryota Bannai <ryotala0528@gmail.com>
2021-07-26 00:22:46 +0900

    add: finished up 'syntax-in-functions' chap

--- SynaxInFunction.hs
+++ SynaxInFunction.hs
@@ -1,4 +1,5 @@
--- Pattern match
+-- Pattern match (pattern matching on function parameters)
+-- Pattern matching in function definitions is syntactic sugar for case expressions
 -- When defining multiple line in ghci, wrap functions lke this :{ [ function definition comes here. ]:}
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
@@ -38,14 +39,36 @@ If all the guards of a function evaluate to False (and we haven't provided an ot
 -}
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell w h
-  | calcBmi w h <= 18.5 = "You're underweight, you emo, you!"
-  | calcBmi w h <= 25.0 = "You're supposedly normal. Pfft, I bet you're ugly!"
-  | calcBmi w h <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pfft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
   | otherwise = "You're a whale, congratulations!"
-
+  where bmi = w / h ^ 2
+        (skinny, normal, fat)  = (18.5, 25.0, 30.0) 
+        
+-- Replaced to Where bindings--
 calcBmi :: (RealFloat a) => a -> a -> a
 calcBmi weight height = weight / height ^ 2
 -- bmiTell 85 1.90
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where bmi weight height = weight / height ^ 2
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+{- 
+calcBmis' [(85, 1.90), (85, 1.90), (85, 1.843)] 
+-}
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where (f:_) = firstname
+        (l:_) = lastname
+        
+initials' :: String -> String -> String
+initials' (f:_) (l:_) = [f] ++ ". " ++ [l] ++ "."
+initials' _ _ = "Please supply two Strings."
 
 {-
 Not only can we call functions as infix with backticks, we can also define them using backticks. Sometimes it's easier to read that way.
@@ -55,3 +78,35 @@ a `myCompare` b
   | a > b = GT
   | a == b = EQ
   | otherwise = LT
+
+
+-- Let Bindings
+-- r: radius h: height
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
+
+{-
+[let square x = x * x in (square 5, square 3, square 2)]
+> [(25,9,4)]
+-}
+
+{-
+Case expressions
+Whereas pattern matching on function parameters can only be done when defining functions, case expressions can be used pretty much anywhere. For instance:
+-}
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [_] -> "a singleton list."
+                                               _ -> "a longer list."
+
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+  -- Below is pattern matching in function definition.
+  where what [] = "empty."
+        what [_] = "a singleton list."
+        what _ = "a longer list."
