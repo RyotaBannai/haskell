935597567df01ee39909bbe7af3a3b7199670e2d
Ryota Bannai <ryotala0528@gmail.com>
2021-07-25 19:25:52 +0900

    add: finished up 'types-and-typeclasses' chap

---
+++ typesAndTypeclasses.hs
@@ -0,0 +1,7 @@
+-- type declaration
+removeNonUpperCase :: String -> String
+-- removeNonUpperCase :: [Char] -> [Char]
+removeNonUpperCase st = [c | c<-st, c `elem` ['A'..'Z']]
+
+addThree:: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
