2b9618d178e5626511663217504b9ddf3e81e8c9
RyotaBannai <ryotala0528@gmail.com>
2021-08-04 23:25:05 +0900

    add: more on IO action with withFile

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,73 @@
 module Main where
-  
-import Control.Monad
-import Data.Char
 
 import qualified Baby
+import Control.Monad
+import Data.Char
+import System.IO
 
 -- main always has a type signate of main :: IO something
 main :: IO ()
-main = interact respondPalindromes
+main = readfwf
 
+readfwf :: IO ()
+readfwf = do
+  withFile
+    "resources/girlfriends.txt"
+    ReadMode
+    ( \handle -> do
+        -- withFile's lambda takes a `handle` and return a I/O action
+        contents <- hGetContents handle
+        putStr contents
+    )
+
+readf :: IO ()
+readf = do
+  -- several I/O actions glued together with a do block
+  handle <- openFile "resources/girlfriends.txt" ReadMode -- if we don't bind that read to a handle, we wouldn't be able to do anything with the file. So in our case, we bound the `handle` to handle
+  contents <- hGetContents handle -- doesn't read it all and stores all. but reads a line as it needs.
+  putStr contents
+  hClose handle
+
+respondPalindromes :: String -> String
 respondPalindromes = unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not palindrome") . lines
-  where isPalindrome xs = xs == reverse xs
+  where
+    isPalindrome xs = xs == reverse xs
 
-intra''' = interact $ unlines . filter ((<10) . length) . lines -- even better impl.
+intra''' :: IO ()
+intra''' = interact $ unlines . filter ((< 10) . length) . lines -- even better impl.
 
+intra'' :: IO ()
 intra'' = interact shortLinesOnly -- better impl. `interact` passes contentsto function(String -> String) and wraps the result String in I/O action.
 
-intra = do 
+intra :: IO ()
+intra = do
   contents <- getContents
   putStr (shortLinesOnly contents)
 
 shortLinesOnly :: String -> String
-shortLinesOnly contents = 
+shortLinesOnly contents =
   let allLines = lines contents
       shortLines = filter (\line -> length line < 10) allLines
       result = unlines shortLines
-  in result
+   in result
 
-
-capsLocker = do 
+capsLocker :: IO ()
+capsLocker = do
   contents <- getContents
   putStr (map toUpper contents)
 
 {-
-`getContents` is I/O lazy, it won't try to read the whole content at once and store it into memory before printing out the capslocked version. 
+`getContents` is I/O lazy, it won't try to read the whole content at once and store it into memory before printing out the capslocked version.
 Rather, it will print out the capslocked version as it reads it, because it will `only read a line from the input when it really needs to`.
 -}
 
-
-fore = forever $ do 
+fore :: IO b
+fore = forever $ do
   putStr "Give me some input: "
   l <- getLine
   putStr $ map toUpper l
 
+gl :: IO ()
 gl = do
   putStrLn "Hello, what's your name?"
   name <- getLine -- bind its result value to `name`
