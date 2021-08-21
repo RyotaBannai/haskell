050f597b92e121eb665b784e087fd8b7aa8bfe4a
RyotaBannai <ryotala0528@gmail.com>
2021-08-22 00:32:51 +0900

    fix: msort

--- src/Ph2/MiscCodes.hs
+++ src/Ph2/MiscCodes.hs
@@ -117,13 +117,22 @@ odds :: [a] -> [a]
 odds [] = []
 odds (_ : xs) = evens xs
 
-halve :: [a] -> [[a]]
-halve [] = []
-halve xs = let mid = (`div` 2) . length $ xs in [take mid xs, drop mid xs]
+halve :: [a] -> ([a], [a])
+halve [] = ([], [])
+halve xs = let mid = (`div` 2) . length $ xs in splitAt mid xs
 
+-- 1. singleton list どうしの比較であれば確実にソート + マージ可能
+-- 2. ソート済みの 2 つのリストであればマージ + ソート可能
+-- merge [1,6,2] [9,5,1] # [1,6,2,9,5,1]
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge (x : xs) (y : ys)
-  | x <= y = x : merge xs (y : ys)
+  | x < y = x : merge xs (y : ys)
   | otherwise = y : merge (x : xs) ys
+
+-- msort [1,6,2,9,5,1] # [1,1,2,5,6,9]
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = let (left, right) = halve xs in merge (msort left) (msort right)
