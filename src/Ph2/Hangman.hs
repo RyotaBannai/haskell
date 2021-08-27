f62487e06a4b1978bb7d1f1324e12f06685796c9
RyotaBannai <ryotala0528@gmail.com>
2021-08-27 23:56:25 +0900

    add: Hangman game codes

---
+++ src/Ph2/Hangman.hs
@@ -0,0 +1,45 @@
+module Ph2.Hangman where
+
+import System.IO (hSetEcho, stdin)
+
+-- hang man
+hangman :: IO ()
+hangman = do
+  putStrLn "Think of a word:"
+  word <- sgetLine
+  putStrLn "Try to guess it:"
+  play word
+
+-- 何が打ち込まれたか隠すためにダッシュ記号 '-' エコーバックする
+sgetLine :: IO String
+sgetLine = do
+  x <- getCh
+  if x == '\n'
+    then do
+      putChar x
+      return []
+    else do
+      putChar '-'
+      xs <- sgetLine
+      return (x : xs) -- `xs` can be null
+
+getCh :: IO Char
+getCh = do
+  hSetEcho stdin False -- echo back 機能を False (入力した文字を repl に表示しない).
+  x <- getChar
+  hSetEcho stdin True
+  return x
+
+-- 二人目の player に推測を入力させ、それを秘密の単語と一致するまで繰り返すというゲームのメインループを実現させる.
+play :: String -> IO ()
+play word = do
+  putStr "? "
+  guess <- getLine
+  if guess == word
+    then putStrLn "You got it!"
+    else do
+      putStrLn (match word guess)
+      play word
+
+match :: String -> String -> String
+match xs ys = [if x `elem` ys then x else '-' | x <- xs]
